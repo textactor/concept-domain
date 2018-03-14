797dc656795e9e265fe6d471852e16dcfd7b3c5e
Dumitru Cantea <canteadumitru@gmail.com>
2018-03-14 17:45:16 +0200

    push concepts use case

---
+++ src/interactors/pushConceptsUseCase.ts
@@ -0,0 +1,15 @@
+
+import { UseCase } from '@textactor/domain';
+import { IConcept } from '../entities/concept';
+import { IConceptWriteRepository } from './conceptRepository';
+
+export class PushConceptsUseCase extends UseCase<IConcept[], void, void> {
+    constructor(private repository: IConceptWriteRepository) {
+        super()
+    }
+
+    protected innerExecute(concepts: IConcept[]): Promise<void> {
+        return Promise.all(concepts.map(concept => this.repository.createOrIncrementPopularity(concept)))
+            .then(_ => null)
+    }
+}
