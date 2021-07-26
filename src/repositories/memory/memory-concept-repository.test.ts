bde47d37fade914f0520dd0a6d704b1390870e55
Dumitru C <canteadumitru@gmail.com>
2021-07-26 21:49:42 +0300

    v0.3.0

--- src/repositories/memory/memory-concept-repository.test.ts
+++ src/repositories/memory/memory-concept-repository.test.ts
@@ -1,44 +1,65 @@
+import { MemoryConceptRepository } from "./memory-concept-repository";
+import test from "ava";
+import { ConceptHelper } from "../../entities/concept-helper";
 
-import { MemoryConceptRepository } from './memory-concept-repository';
-import test from 'ava';
-import { ConceptHelper } from '../../entities/concept-helper';
+test("#create", async (t) => {
+  const repository = new MemoryConceptRepository();
+  const concept = ConceptHelper.build({
+    name: "New York",
+    abbr: "NY",
+    country: "us",
+    lang: "en",
+    containerId: "1"
+  });
 
-test('#create', async t => {
-    const repository = new MemoryConceptRepository();
-    const concept = ConceptHelper.build({ name: 'New York', abbr: 'NY', country: 'us', lang: 'en', containerId: '1' });
+  const createdConcept = await repository.create(concept);
 
-    const createdConcept = await repository.create(concept);
+  t.is(createdConcept.name, concept.name);
+  t.is(createdConcept.abbr, concept.abbr);
 
-    await t.throws(repository.create(concept), /Item already exists/i);
+  concept.name = "New Name";
 
-    t.is(createdConcept.name, concept.name);
-    t.is(createdConcept.abbr, concept.abbr);
+  t.not(createdConcept.name, concept.name);
+});
 
-    concept.name = 'New Name';
+test("#getById", async (t) => {
+  const repository = new MemoryConceptRepository();
+  const concept = ConceptHelper.build({
+    name: "New York",
+    abbr: "NY",
+    country: "us",
+    lang: "en",
+    containerId: "1"
+  });
+  await repository.create(concept);
+  const concept1 = await repository.getById(concept.id);
+  t.true(!!concept1);
 
-    t.not(createdConcept.name, concept.name);
-})
+  const concept2 = await repository.getById("fake");
+  t.false(!!concept2);
+});
 
-test('#getById', async t => {
-    const repository = new MemoryConceptRepository();
-    const concept = ConceptHelper.build({ name: 'New York', abbr: 'NY', country: 'us', lang: 'en', containerId: '1' });
-    await repository.create(concept);
-    const concept1 = await repository.getById(concept.id);
-    t.true(!!concept1);
+test("#getByIds", async (t) => {
+  const repository = new MemoryConceptRepository();
+  const concept1 = ConceptHelper.build({
+    name: "New York",
+    abbr: "NY",
+    country: "us",
+    lang: "en",
+    containerId: "1"
+  });
+  await repository.create(concept1);
+  const concept2 = ConceptHelper.build({
+    name: "New York City",
+    abbr: "NY",
+    country: "us",
+    lang: "en",
+    containerId: "1"
+  });
+  await repository.create(concept2);
+  const concepts = await repository.getByIds([concept1.id, concept2.id]);
 
-    const concept2 = await repository.getById('fake');
-    t.false(!!concept2);
-})
-
-test('#getByIds', async t => {
-    const repository = new MemoryConceptRepository();
-    const concept1 = ConceptHelper.build({ name: 'New York', abbr: 'NY', country: 'us', lang: 'en', containerId: '1' });
-    await repository.create(concept1);
-    const concept2 = ConceptHelper.build({ name: 'New York City', abbr: 'NY', country: 'us', lang: 'en', containerId: '1' });
-    await repository.create(concept2);
-    const concepts = await repository.getByIds([concept1.id, concept2.id]);
-
-    t.is(concepts.length, 2);
-    t.is(concepts[0].id, concept1.id);
-    t.is(concepts[1].id, concept2.id);
-})
+  t.is(concepts.length, 2);
+  t.is(concepts[0].id, concept1.id);
+  t.is(concepts[1].id, concept2.id);
+});
--- src/repositories/memory/memory-concept-repository.ts
+++ src/repositories/memory/memory-concept-repository.ts
@@ -1,203 +1,221 @@
+import { Concept } from "../../entities/concept";
+import { Locale } from "../../types";
+import {
+  ConceptRepository,
+  PopularConceptsOptions
+} from "../concept-repository";
+import { MemoryRepository } from "./memory-repository";
+import { uniqByProperty, uniq } from "@textactor/domain";
 
-import { Concept } from '../../entities/concept';
-import { Locale } from '../../types';
-import { ConceptRepository, PopularConceptsOptions } from '../concept-repository';
-import { MemoryRepository } from './memory-repository';
-import { uniqByProperty, uniq } from '@textactor/domain';
+export class MemoryConceptRepository
+  extends MemoryRepository<Concept>
+  implements ConceptRepository
+{
+  constructor() {
+    super();
+  }
 
-export class MemoryConceptRepository extends MemoryRepository<Concept> implements ConceptRepository {
-    constructor() {
-        super()
+  getMostPopular(
+    containerId: string,
+    limit: number,
+    skip: number,
+    options?: PopularConceptsOptions
+  ) {
+    options = { ...options };
+    const list: Concept[] = [];
+    for (let item of this.db.values()) {
+      if (item.containerId !== containerId) {
+        continue;
+      }
+      if (options.minCountWords && item.countWords < options.minCountWords) {
+        continue;
+      }
+      if (options.maxCountWords && item.countWords > options.maxCountWords) {
+        continue;
+      }
+      list.push(item);
+    }
+    return Promise.resolve(list.slice(skip, skip + limit));
+  }
+
+  async getByRootNameIds(ids: string[]): Promise<Concept[]> {
+    let list: Concept[] = [];
+
+    for (let id of ids) {
+      const concepts = await this.getByRootNameId(id);
+      list = list.concat(concepts);
     }
 
-    getMostPopular(containerId: string, limit: number, skip: number, options?: PopularConceptsOptions) {
-        options = { ...options };
-        const list: Concept[] = [];
-        for (let item of this.db.values()) {
-            if (item.containerId !== containerId) {
-                continue;
-            }
-            if (options.minCountWords && item.countWords < options.minCountWords) {
-                continue;
-            }
-            if (options.maxCountWords && item.countWords > options.maxCountWords) {
-                continue;
-            }
-            list.push(item);
-        }
-        return Promise.resolve(list.slice(skip, skip + limit));
+    return uniqByProperty(list, "id");
+  }
+
+  count(locale: Locale): Promise<number> {
+    let count = 0;
+    for (let item of this.db.values()) {
+      if (item.country !== locale.country || item.lang !== locale.lang) {
+        continue;
+      }
+
+      count++;
     }
 
-    async getByRootNameIds(ids: string[]): Promise<Concept[]> {
-        let list: Concept[] = [];
+    return Promise.resolve(count);
+  }
 
-        for (let id of ids) {
-            const concepts = await this.getByRootNameId(id);
-            list = list.concat(concepts);
-        }
-
-        return uniqByProperty(list, 'id');
+  getConceptsWithAbbr(containerId: string): Promise<Concept[]> {
+    const list: Concept[] = [];
+    for (let item of this.db.values()) {
+      if (item.containerId === containerId && !item.isAbbr && item.abbr) {
+        list.push(item);
+      }
     }
 
-    count(locale: Locale): Promise<number> {
-        let count = 0;
-        for (let item of this.db.values()) {
-            if (item.country !== locale.country || item.lang !== locale.lang) {
-                continue;
-            }
+    return Promise.resolve(list);
+  }
 
-            count++;
-        }
-
-        return Promise.resolve(count);
+  async deleteByRootNameIds(ids: string[]): Promise<number> {
+    const list = await this.getByRootNameIds(ids);
+    let count = list.length;
+    for (let item of list) {
+      this.db.delete(item.id);
     }
 
-    getConceptsWithAbbr(containerId: string): Promise<Concept[]> {
-        const list: Concept[] = []
-        for (let item of this.db.values()) {
-            if (item.containerId === containerId && !item.isAbbr && item.abbr) {
-                list.push(item);
-            }
-        }
+    return Promise.resolve(count);
+  }
 
-        return Promise.resolve(list);
+  list(locale: Locale, limit: number, skip?: number): Promise<Concept[]> {
+    skip = skip || 0;
+    const list: Concept[] = [];
+    for (let item of this.db.values()) {
+      if (item.country !== locale.country || item.lang !== locale.lang) {
+        continue;
+      }
+      list.push(item);
     }
 
-    async deleteByRootNameIds(ids: string[]): Promise<number> {
+    return Promise.resolve(list.slice(skip, skip + limit));
+  }
+  async create(data: Concept): Promise<Concept> {
+    if (!!this.db.get(data.id)) {
+      return Promise.reject(new Error(`Item already exists!`));
+    }
+    this.db.set(data.id, {
+      ...{ popularity: 1, createdAt: Date.now() },
+      ...data
+    });
 
-        const list = await this.getByRootNameIds(ids);
-        let count = list.length;
-        for (let item of list) {
-            this.db.delete(item.id);
-        }
+    const entity = await this.getById(data.id);
+    if (!entity) {
+      throw new Error(`Entity not found!`);
+    }
+    return entity;
+  }
 
-        return Promise.resolve(count);
+  getByRootNameId(id: string): Promise<Concept[]> {
+    const list: Concept[] = [];
+    for (let item of this.db.values()) {
+      if (~item.rootNameIds.indexOf(id)) {
+        list.push(item);
+      }
     }
 
-    list(locale: Locale, limit: number, skip?: number): Promise<Concept[]> {
-        skip = skip || 0;
-        const list: Concept[] = []
-        for (let item of this.db.values()) {
-            if (item.country !== locale.country || item.lang !== locale.lang) {
-                continue;
-            }
-            list.push(item)
-        }
-
-        return Promise.resolve(list.slice(skip, skip + limit));
-    }
-    async create(data: Concept): Promise<Concept> {
-        if (!!this.db.get(data.id)) {
-            return Promise.reject(new Error(`Item already exists!`));
-        }
-        this.db.set(data.id, { ...{ popularity: 1, createdAt: Date.now() }, ...data });
-
-        const entity = await this.getById(data.id);
-        if (!entity) {
-            throw new Error(`Entity not found!`)
-        }
-        return entity;
+    return Promise.resolve(list);
+  }
+  deleteUnpopular(containerId: string, popularity: number): Promise<number> {
+    let count = 0;
+    for (let item of this.db.values()) {
+      if (item.containerId !== containerId) {
+        continue;
+      }
+      if (item.popularity <= popularity) {
+        this.db.delete(item.id) && count++;
+      }
     }
 
-    getByRootNameId(id: string): Promise<Concept[]> {
-        const list: Concept[] = []
-        for (let item of this.db.values()) {
-            if (~item.rootNameIds.indexOf(id)) {
-                list.push(item)
-            }
-        }
-
-        return Promise.resolve(list);
-    }
-    deleteUnpopular(containerId: string, popularity: number): Promise<number> {
-        let count = 0;
-        for (let item of this.db.values()) {
-            if (item.containerId !== containerId) {
-                continue;
-            }
-            if (item.popularity <= popularity) {
-                this.db.delete(item.id) && count++;
-            }
-        }
-
-        return Promise.resolve(count);
-    }
-    deleteUnpopularAbbreviations(containerId: string, popularity: number): Promise<number> {
-        let count = 0;
-        for (let item of this.db.values()) {
-            if (item.containerId !== containerId) {
-                continue;
-            }
-            if (item.isAbbr && item.popularity <= popularity) {
-                this.db.delete(item.id) && count++;
-            }
-        }
-
-        return Promise.resolve(count);
-    }
-    deleteUnpopularOneWorlds(containerId: string, popularity: number): Promise<number> {
-        let count = 0;
-        for (let item of this.db.values()) {
-            if (item.containerId !== containerId) {
-                continue;
-            }
-            if (item.countWords === 1 && item.popularity <= popularity) {
-                this.db.delete(item.id) && count++;
-            }
-        }
-
-        return Promise.resolve(count);
-    }
-    deleteAll(containerId: string): Promise<number> {
-        let count = 0;
-        for (let item of this.db.values()) {
-            if (item.containerId !== containerId) {
-                continue;
-            }
-
-            this.db.delete(item.id) && count++;
-        }
-
-        return Promise.resolve(count);
-    }
-    deleteIds(ids: string[]): Promise<number> {
-        let count = 0;
-        for (let id of ids) {
-            this.db.delete(id) && count++;
-        }
-
-        return Promise.resolve(count);
-    }
-    incrementPopularity(id: string): Promise<number> {
-        const item = this.db.get(id);
-
-        if (!item) {
-            return Promise.resolve(0);
-        }
-        item.popularity++;
-
-        return Promise.resolve(item.popularity);
-    }
-    async createOrUpdate(concept: Concept): Promise<Concept> {
-        concept = { ...concept };
-        const id = concept.id;
-        let item = this.db.get(id);
-        if (!item) {
-            item = await this.create(concept);
-        } else {
-            item.popularity++;
-            item.rootNameIds = uniq(item.rootNameIds.concat(concept.rootNameIds));
-        }
-
-        return Promise.resolve(item);
+    return Promise.resolve(count);
+  }
+  deleteUnpopularAbbreviations(
+    containerId: string,
+    popularity: number
+  ): Promise<number> {
+    let count = 0;
+    for (let item of this.db.values()) {
+      if (item.containerId !== containerId) {
+        continue;
+      }
+      if (item.isAbbr && item.popularity <= popularity) {
+        this.db.delete(item.id) && count++;
+      }
     }
 
-    all(): Promise<Concept[]> {
-        const array: Concept[] = []
-        for (let item of this.db.values()) {
-            array.push(item);
-        }
-
-        return Promise.resolve(array);
+    return Promise.resolve(count);
+  }
+  deleteUnpopularOneWorlds(
+    containerId: string,
+    popularity: number
+  ): Promise<number> {
+    let count = 0;
+    for (let item of this.db.values()) {
+      if (item.containerId !== containerId) {
+        continue;
+      }
+      if (item.countWords === 1 && item.popularity <= popularity) {
+        this.db.delete(item.id) && count++;
+      }
     }
+
+    return Promise.resolve(count);
+  }
+  deleteAll(containerId: string): Promise<number> {
+    let count = 0;
+    for (let item of this.db.values()) {
+      if (item.containerId !== containerId) {
+        continue;
+      }
+
+      this.db.delete(item.id) && count++;
+    }
+
+    return Promise.resolve(count);
+  }
+  deleteIds(ids: string[]): Promise<number> {
+    let count = 0;
+    for (let id of ids) {
+      this.db.delete(id) && count++;
+    }
+
+    return Promise.resolve(count);
+  }
+  incrementPopularity(id: string): Promise<number> {
+    const item = this.db.get(id);
+
+    if (!item) {
+      return Promise.resolve(0);
+    }
+    item.popularity++;
+
+    return Promise.resolve(item.popularity);
+  }
+  async createOrUpdate(concept: Concept): Promise<Concept> {
+    concept = { ...concept };
+    const id = concept.id;
+    let item = this.db.get(id);
+    if (!item) {
+      item = await this.create(concept);
+    } else {
+      item.popularity++;
+      item.rootNameIds = uniq(item.rootNameIds.concat(concept.rootNameIds));
+    }
+
+    return Promise.resolve(item);
+  }
+
+  all(): Promise<Concept[]> {
+    const array: Concept[] = [];
+    for (let item of this.db.values()) {
+      array.push(item);
+    }
+
+    return Promise.resolve(array);
+  }
 }
